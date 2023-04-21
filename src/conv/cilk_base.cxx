9589f29b9d956fc61df1cd3e86485dc9566353fa
Xuhao Chen <cxh@mit.edu>
2023-04-21 18:51:48 -0400

    add conv cilk

---
+++ src/conv/cilk_base.cxx
@@ -0,0 +1,43 @@
+#include <omp.h>
+#include <stdlib.h>
+#include <cilk/cilk.h>
+#include <cilk/cilk_api.h>
+
+void convolutionRows(float *Dst, float *Src, float *Kernel,
+                     int imageW, int imageH, int kernelR) {
+  cilk_for (int y = 0; y < imageH; y++) {
+    cilk_for (int x = 0; x < imageW; x++) {
+      double sum = 0;
+      for (int k = -kernelR; k <= kernelR; k++){
+        int d = x + k;
+        if (d >= 0 && d < imageW)
+          sum += Src[y * imageW + d] * Kernel[kernelR - k];
+      }
+      Dst[y * imageW + x] = (float)sum;
+    }
+  }
+}
+
+void convolutionColumns(float *Dst, float *Src, float *Kernel,
+                        int imageW, int imageH, int kernelR) {
+  cilk_for (int y = 0; y < imageH; y++) {
+    cilk_for (int x = 0; x < imageW; x++) {
+      double sum = 0;
+      for (int k = -kernelR; k <= kernelR; k++){
+        int d = y + k;
+        if (d >= 0 && d < imageH)
+          sum += Src[d * imageW + x] * Kernel[kernelR - k];
+      }
+      Dst[y * imageW + x] = (float)sum;
+    }
+  }
+}
+
+extern "C"
+void convolution(float *Dst, float *Src, float *Kernel,
+    int imageW, int imageH, int kernelR) {
+  float* h_Buffer = (float*)malloc(imageW * imageH * sizeof(float));
+  convolutionRows(h_Buffer, Src, Kernel, imageW, imageH, imageW);
+  convolutionColumns(Dst, h_Buffer, Kernel, imageW, imageH, imageW);
+  free(h_Buffer);
+}
